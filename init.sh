8822b49c6b1bed050f8f94156692bb7d99058cb7
Baptiste Pasquier <pasquierbaptiste@hotmail.fr>
2021-04-02 22:28:09 +0000

    CSV LR

--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
 cd "work/"
 git clone https://github.com/baptiste-pasquier/statapp/
 
-wget --user-agent Mozilla/4.0 'https://transfer.sh/get/6GCE0/data.zip'
+wget --user-agent Mozilla/4.0 'https://transfer.sh/get/LDUT3/data.zip'
 unzip data.zip
 mv -v "data" "statapp/data"
 rm data.zip
